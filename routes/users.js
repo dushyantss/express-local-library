7fc63486c84dd7213a5ee3b6c588a605d9bddd42
Dushyant Singh Shekhawat <contact@dushyantss.com>
2019-04-22 17:13:15 +0530

    Finish first challenge
    Signed-off-by: Dushyant Singh Shekhawat <contact@dushyantss.com>

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+// Cool users challenge
+router.get('/cool', function(req, res, next) {
+  res.send('Your\'re so cool');
+});
+
 module.exports = router;
